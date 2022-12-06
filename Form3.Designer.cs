669ad4bea44f9e40b115a5a08f3c75ca696a1077
Osama-Moh <usama.nasser21@gmail.com>
2022-12-06 18:47:50 +0200

    complains form

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -112,6 +112,7 @@ namespace Project
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
             "Yes",
@@ -134,6 +135,7 @@ namespace Project
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Delivery issue",
