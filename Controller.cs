6a7b4a88def281e22d203289942d6ec1e3f6e2d8
Osama-Moh <usama.nasser21@gmail.com>
2022-12-08 18:22:55 +0200

    add controller, app.config,dbmanager

---
+++ Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+        //TODO: functions in the requirement
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+    }
+}
