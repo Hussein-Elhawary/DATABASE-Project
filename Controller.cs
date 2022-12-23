d66ae56b8f8c65ac4c956f2b95f433ebe9ef6959
Festivef2p <96025586+Festivef2p@users.noreply.github.com>
2022-12-23 11:14:46 +0200

    for 7esen

--- Controller.cs
+++ Controller.cs
@@ -408,8 +408,15 @@ namespace Project
         {
             string storedproc = StoredProcedures.Select_TotalCM_Unresolved;
             Dictionary<string, object> Parameters = new Dictionary<string, object>();
-          
+
             return dbMan.ExecuteReader(storedproc, Parameters);
         }
+
+        public DataTable ViewOrderDetails(string order_num)
+        {
+            string query = "SELECT P.Name,P.Price,C.Quantity as 'Amount',P.Price * C.Quantity as 'Total' from [contains] C,Products P WHERE C.[Product ID] = P.[Product ID] and C.[Order ID] = '" + order_num + "';";
+            return dbMan.ExecuteReader(query);
+        }
+
     }
 }
--- Customer_View_Orders.cs
+++ Customer_View_Orders.cs
@@ -33,14 +33,30 @@ namespace Project
         }
 
         private void Customer_View_Orders_Load(object sender, EventArgs e)
-        {
+        {        
             controllerDB = new Controller();
             DataTable DT = controllerDB.GetNumberofOrdersForSelectedCustomer(Cust_ID);
             OrderCount_Label.Text = DT.Rows[0][0].ToString();
             controllerDB = new Controller();
             DT = controllerDB.GetOrdersforSelectedCustomer(Cust_ID);
             dataGridView1.DataSource = DT;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = "Click To View";
+            }
             dataGridView1.Refresh();
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int Order = e.RowIndex;
+            OrderDetails a = new OrderDetails(dataGridView1.Rows[Order].Cells["Order Number"].Value.ToString());
+            a.Show();
         }
     }
 }
